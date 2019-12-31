d3069eaf4b3919a3c33070396d67efb646843b77
mohamedsayed18 <mohamed95.a.s@gmail.com>
2019-12-31 09:54:07 +0300

    docstrings

--- conway.py
+++ conway.py
@@ -1,50 +1,56 @@
+"""Conway game of life there are two classes ConWay, GolGui"""
 import numpy as np
 import tkinter as tk
 
-class ConWay :
+
+class ConWay:
+    """"""
     def __init__(self):
+        """Create the play ground"""
         self.pg = np.zeros((20, 20))  # size of the playground
         self.new_pg = np.zeros((20, 20))
 
-    def count_neighbours(self,row,col):
+    def count_neighbours(self, row, col):
+        """Returns number of living neighbours for a given cell"""
         lives = 0
-        for i in range(row-1,row+2):
-            for j in range(col-1,col+2):
+        for i in range(row-1, row+2):
+            for j in range(col-1, col+2):
                 if i in range(self.pg.shape[0]) and j in range(self.pg.shape[1]) and (i != row or j != col):
                     lives += self.pg[i][j]
-                    #print("alive",i,j)
         return lives
 
     def new_screen(self):
-        for i in range(self.pg.shape[0]): # loop through rows
+        """Update the playground"""
+        for i in range(self.pg.shape[0]):   # loop through rows
             for j in range(self.pg.shape[1]):   # loop through cols
                 population = self.count_neighbours(i, j)  # population of neighbours
                 # check if cell is alive or dead
                 if self.pg[i][j]:    # alive
                     if population == 2 or population == 3:
                         self.new_pg[i][j] = 1
-                else:   #dead
+                else:   # dead
                     if population == 3:
                         self.new_pg[i][j] = 1
 
-        self.pg = self.new_pg
-        self.new_pg = np.zeros((20, 20))
+        self.pg = self.new_pg   # Assign the new playground to old one
+        self.new_pg = np.zeros((20, 20))    # clear the playground
 
-    def shapes (self, shape):
+    def shapes(self, shape):
+        """Generate the given shape Blinker,Toad,Glider or Beehive"""
         if shape == 'Blinker':
             # Blinker
-            self.pg[1,1]=1
-            self.pg[1,2]=1
-            self.pg[1,3]=1
+            self.pg[1, 1] = 1
+            self.pg[1, 2] = 1
+            self.pg[1, 3] = 1
         elif shape == 'Toad':
             # Toad
-            self.pg[1,2]=1
-            self.pg[1,3]=1
-            self.pg[1,4]=1
+            self.pg[1, 2] = 1
+            self.pg[1, 3] = 1
+            self.pg[1, 4] = 1
 
-            self.pg[2,1]=1
-            self.pg[2,2]=1
-            self.pg[2,3]=1
+            self.pg[2, 1] = 1
+            self.pg[2, 2] = 1
+            self.pg[2, 3] = 1
         elif shape == 'Glider':
             # Glider
             self.pg[1, 1] = 1
@@ -53,14 +59,16 @@ class ConWay :
             self.pg[3, 1] = 1
             self.pg[3, 2] = 1
         elif shape == 'Beehive':
-            self.pg[3, 5] = 1
-            self.pg[3, 6] = 1
-            self.pg[4, 4] = 1
-            self.pg[4, 7] = 1
-            self.pg[5, 5] = 1
-            self.pg[5, 6] = 1
+            self.pg[13, 5] = 1
+            self.pg[13, 6] = 1
+            self.pg[14, 4] = 1
+            self.pg[14, 7] = 1
+            self.pg[15, 5] = 1
+            self.pg[15, 6] = 1
+
 
 class GolGui:
+    """GUI of game draw the grid and the shapes"""
     def __init__(self, master):
         self.master = master  # create the master window
         master.title("simple GUI")
@@ -70,40 +78,28 @@ class GolGui:
         self.c.pack(fill=tk.BOTH, expand=True)  # chose some options related to canvas (widget)
         self.c.bind('<Configure>', self.create_grid)
 
-    def create_grid(self,event=None):
-        """draw the grid 9*9"""
+    def create_grid(self, event=None):
+        """Draw the grid"""
         w = self.c.winfo_width()  # Get current width of canvas
         h = self.c.winfo_height()  # Get current height of canvas
-        #self.c.delete('grid_line')  # Will only remove the grid_line
 
-        for i in range(9):
+        for i in range(h):
             self.c.create_line([(i * 30, 0), (i * 30, h)], tag='grid_line')
 
-        for i in range(9):
+        for i in range(w):
             self.c.create_line([(0, i * 30), (w, i * 30)], tag='grid_line')
 
-    def color_rec(self, col, row ):
+    def color_rec(self, col, row):
+        """Draw one rectangle at row, col"""
         start = (row * 30, col * 30)
         end = (row * 30 + 30, col * 30 + 30)
         self.c.create_rectangle([start, end], fill="black", outline="red")
 
     def draw(self, a):
+        """Draw the new playground"""
         self.c.delete("all")
         self.create_grid()
+        # loop through the play ground and draw the rectangles
         for ix, iy in np.ndindex(a.shape):
-            if a[ix,iy] == 1:
-                self.color_rec(ix,iy)
-                print("found @")
-
-
-
-if __name__ == "__main__":
-    play = ConWay()
-    #play.pg[0][0]=1
-    play.pg[0][1]=1
-    play.pg[1][0]=1
-    play.pg[1][1]=1
-    print(play.pg)
-    play.new_screen()
-    print(play.pg)
-    #print(lives)
+            if a[ix, iy] == 1:
+                self.color_rec(ix, iy)
--- trial.py
+++ trial.py
@@ -2,17 +2,18 @@ import tkinter as tk
 
 from conway import ConWay, GolGui
 
+
+def make_iteration():
+    gou.draw(play.pg)   # draw the playground
+    play.new_screen()   # calculate the new playground
+    root.after(500, make_iteration) # call the function again after 500 ms
+
+
 play = ConWay()  # create a game instance
 root = tk.Tk()  # create the master window
 gou = GolGui(root)  # instance of my GUI
-play.shapes('Beehive')  #choose the shape to start with
+play.shapes('Beehive')  # choose the shape to start with
 play.shapes('Toad')
-
-def make_iteration():
-    gou.draw(play.pg)   #draw the playground
-    play.new_screen()   #calculate the new playground
-    # print("hello")
-    root.after(500, make_iteration) #call the function again after 500 ms
 
 
 make_iteration()
